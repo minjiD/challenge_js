64de96e6323b6d9a267c24329fad06e5bef41c84
minjiD <ssosso12347@gmail.com>
2022-08-05 16:08:48 +0900

    resize, change background color

--- src/index.js
+++ src/index.js
@@ -1,56 +1,18 @@
-let h1 = document.querySelector("h1");
-let color = ["#FC3E60", "#52FAB4", "#4BD4FA", "#729CFC"];
+const h1 = document.querySelector("h1");
+const body = document.querySelector("body");
+const color = ["#2EE6D6", "#36D96F", "#C3E62E"];
 
-/* 
-//방식 1
-const superEventHandle = {
-  textEnter: function () {
-    h1.innerText = "The mouse is here";
-    h1.style.color = color[0];
-  },
-  textLeave: function () {
-    h1.innerText = "The mouse is gone";
-    h1.style.color = color[1];
-  },
-  winResize: function () {
-    h1.innerText = "You just resized!";
-    h1.style.color = color[2];
-  },
-  winCon: function () {
-    h1.innerText = "That was a right click!";
-    h1.style.color = color[3];
-  }
-};
+h1.style.color = "white";
+// body.style.backgroundColor = "#2EE6D6";
 
-h1.addEventListener("mouseenter", superEventHandle.textEnter);
-h1.addEventListener("mouseleave", superEventHandle.textLeave);
-window.addEventListener("resize", superEventHandle.winResize);
-window.addEventListener("contextmenu", superEventHandle.winCon);
-*/
-
-//방식 2
-const ENTERTEXT = "The mouse is here";
-const LEAVETEXT = "The mouse is gone";
-const RESIXEWIN = "You just resized!";
-const CONWIN = "That was a right click!";
-
-const superEventHandle = {
-  eventFun: function (textCon, txtColor) {
-    h1.innerText = textCon;
-    h1.style.color = txtColor;
-  }
-};
-
-h1.addEventListener("mouseenter", () => {
-  superEventHandle.eventFun(ENTERTEXT, color[0]);
+window.addEventListener("resize", function() {
+    let winWidth = window.innerWidth;
+    //console.log(winWidth);
+    if(winWidth <= 800){
+        body.style.backgroundColor = color[0];
+    } else if(winWidth > 800 && winWidth <= 1500) {
+        body.style.backgroundColor = color[1];
+    } else if(winWidth > 1500) {
+        body.style.backgroundColor = color[2];
+    }
 });
-h1.addEventListener("mouseleave", () => {
-  superEventHandle.eventFun(LEAVETEXT, color[1]);
-});
-window.addEventListener("resize", () => {
-  superEventHandle.eventFun(RESIXEWIN, color[2]);
-});
-window.addEventListener("contextmenu", () => {
-  superEventHandle.eventFun(CONWIN, color[3]);
-});
-
