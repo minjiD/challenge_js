07f84e6167421c5ffdc8c61b90ffeefd5d642ec8
minjiD <ssosso12347@gmail.com>
2022-08-10 19:11:59 +0900

    date, time comment

--- src/index_04.js
+++ src/index_04.js
@@ -5,16 +5,20 @@ function getClock() {
     const christmasDate = new Date("2022-12-25 00:00:00");
 
     let diffDate = christmasDate - today; //기간 객체
+    
     //Date
     let diffDay = Math.floor((christmasDate.getTime() - today.getTime())/(1000 * 60 * 60 * 24));
-    diffDate -= diffDay * (1000 * 60 * 60 * 24);
+
     //Hour
+    diffDate -= diffDay * (1000 * 60 * 60 * 24); //현재 시간을 구하기 위해 구한 일수를 밀리세컨즈로 변경해서 기간에서 뺀다.
     let diffHour = Math.floor(diffDate/(1000 * 60 * 60));
-    diffDate -= diffHour * (1000 * 60 * 60);
+    
     //Minutes
+    diffDate -= diffHour * (1000 * 60 * 60); //현재 분을 구하기 위해 구한 시간를 밀리세컨즈로 변경해서 기간에서 뺀다.
     let diffMin = Math.floor(diffDate/(1000 * 60));
-    diffDate -= diffMin * (1000 * 60);
+    
     //Second
+    diffDate -= diffMin * (1000 * 60); //현재 초를 구하기 위해 구한 분을 밀리세컨즈로 변경해서 기간에서 뺀다.
     let diffSec = Math.floor(diffDate/1000);
     diffDate -= diffSec * 1000;
 
